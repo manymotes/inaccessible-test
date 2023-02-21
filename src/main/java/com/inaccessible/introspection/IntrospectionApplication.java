2bd21c068dec2e877f20ba8b6de0f2a03464ce5b
kendallmotes <81644367+kendallmotes@users.noreply.github.com>
2023-02-20 17:57:59 -0700

    init

---
+++ src/main/java/com/inaccessible/introspection/IntrospectionApplication.java
@@ -0,0 +1,13 @@
+package com.inaccessible.introspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrospectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrospectionApplication.class, args);
+	}
+
+}
